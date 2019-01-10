d3a4d6b341c79256c87bd890541b55a82092ea0a
Bobby Patterson <bobbypatterson@bobby-mbp.localdomain>
2019-01-10 15:53:24 -0800

    fileids.js

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,10 @@
-const express = require('express');
-const app = express();
+var http = require('http');
+var fs = require('fs');
+
+var file = fs.createWriteStream("/file.jpg");
+var request = http.get("http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg", function(response) {
+  response.pipe(file);
+});
 
 
 
