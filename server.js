8b30da09135294510ba5288552326089332e10c2
Bobby <blipco619@gmail.com>
2019-01-15 18:01:53 -0800

    deletes download-pdf, installs file-type, updates gitignore

--- server.js
+++ server.js
@@ -1,35 +1,33 @@
 const axios = require('axios');
 const fs = require('fs');
-require('dotenv').config()
+require('dotenv').config();
+const fileType = require('file-type');
 const fileIds = require('./fileIds');
 
 for(let i=0;i<=fileIds.length;i++) {
-
-    axios({
+axios({
         url:`https://api.goformz.com:443/v2/files/${fileIds[i]}`,
         method: 'get',
-        headers: {'Authorization':`Bearer ${process.env.BEARER}`},
-
+        headers: {'Authorization':`Basic ZXJpbi5rYXJ3ZWlrQGFsaW1ha2dyb3VwLmNvbTpnb2Zvcm16MQ==`},
     })
     .then((response) => {
         axios({
-            url: response.data.link,
+            url: `${response.data.link}`,
             method: 'get',
-            headers: {'Authorization':`Bearer ${process.env.BEARER}`}
+            responseType: 'arraybuffer'
         })
-        .then((response) => {
-            fs.writeFile(`./backgroundPdfs/${fileIds[i]}.pdf`, response, (error) => {
+        .then((response1) => {
+            console.log('filetype',fileType(response1.data).ext)
+            fs.writeFile(`./backgroundFiles/${fileIds[i]}.${fileType(response1.data).ext}`,response1.data, (error) => {
                 if (error) console.log('fs.writeFile Error', error);
                     console.log('fs.WriteFile Saved!');
             });
         })
         .catch((error) => {
-            console.log('Second Axios Call Error',error);
-        })
-        
+            console.log('Second Axios Call',error);
+        });
     })
     .catch((error) => {
-        console.log('First Axios Call Error',error);
+        console.log('First Axios Call',error);
     });
-
 };
